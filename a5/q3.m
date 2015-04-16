2c0fb3dcebe2cd685717595dbf00b2f85575ac2f
ACAHNN <acahn@wisc.edu>
2015-04-15 23:43:43 -0500

    commiting the code for q3 and results of q3

---
+++ a5/q3.m
@@ -0,0 +1,27 @@
+m = length(A);
+[P,D] = eig(A);
+D= diag(D);
+
+% Step 1: Power Method to find the first two dominant eigenvalues
+
+% most dominant eigenvalue
+[eval1,evec1,err1] = power_method(A,rand(m,1),10^-2);
+fprintf('1st dom from pm: %f\ntrue 1st dom: %f\n\n',eval1,D(1));
+% second most dominant eigenvalue (deflate eval1 from A)
+e = [1 zeros(1,m-1)]'; w = e-evec1; w = w/norm(w);
+H = eye(m)-2*w*w';
+A1 = H*A*H; tilA = A1(2:end,2:end);
+[eval2,evec2,err2] = power_method(tilA,rand(m-1,1),10^-2);
+fprintf('2nd dom from pm: %f\ntrue 2nd dom: %f\n\n',eval2,D(2));
+
+% Step 2: Inverse Power Method to find the first two least dominant evals
+
+% least dominant eigenvalue
+[eval1,evec1,err1] = inv_power_method(A,rand(m,1),10^-2);
+fprintf('1st least dom from pm: %f\ntrue 1st least dom: %f\n\n',eval1,D(m));
+% second least dominant eigenvalue (deflate eval1 from A)
+e = [1 zeros(1,m-1)]'; w = e-evec1; w = w/norm(w);
+H = eye(m)-2*w*w';
+A1 = H*A*H; tilA = A1(2:end,2:end);
+[eval2,evec2,err2] = inv_power_method(tilA,rand(m-1,1),10^-2);
+fprintf('2nd least dom from pm: %f\ntrue 2nd least dom: %f\n\n',eval2,D(m-1));
